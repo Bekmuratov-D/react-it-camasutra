e38281ac2cc0865430515810b992e94905d29a7b
Bekmuratov-MBF <bekmuratovdilmurat18@gmail.com>
2023-01-21 21:04:01 +0300

    commit1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <div>Footer1</div>
+        </footer>
+    )
+}
+export default Footer
